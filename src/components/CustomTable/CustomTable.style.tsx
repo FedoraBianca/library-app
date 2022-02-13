62aa853c98f929c7528b6f32e749b4429528fa81
Fedora Bianca <fedorabianca@Fedoras-MacBook-Pro.local>
2022-02-13 21:32:10 +0200

    tabel listing

--- src/components/CustomTable/CustomTable.style.tsx
+++
@@ -1,5 +0,0 @@
-import { Table } from 'reactstrap';
-import styled from 'styled-components';
-
-export const CustomTableWrapper = styled(Table)`
-`;
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,21 +1,63 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import CustomTable from "../components/CustomTable";
+import { Card, CardBody, Table } from "reactstrap";
+import CustomButton from "../components/CustomButton";
 import { IState } from "../interfaces/state";
+import { IUser } from "../interfaces/user";
 import { getUserListStart } from "../redux/actions/usersActions";
 
 const DashboardPage = () => {
     const dispatch = useDispatch();
     const { list, loading, error } = useSelector((state: IState) => state.users);
+    const headerData = ['Id', 'Name', 'Username', 'City', 'Email', 'Edit', 'Delete'];
 
     useEffect(() => {
         dispatch(getUserListStart());
     }, []);
 
+    const handleAdd = () => {
+        console.log('add');
+    };
+
+    const handleEdit = (id: number) => {
+        console.log('edit', id);
+    };
+
+    const handleDelete = (id: number) => {
+        console.log('delete', id);
+    };
+
     return (
-        <div>
-            <CustomTable />
-        </div>
+        <Card>
+            <CardBody>
+            <Table className='responsive'>
+                <thead>
+                    <tr>
+                        {
+                            headerData.map((header: string) => <th key={header}>{header}</th>)
+                        }
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        list.map((user: IUser, rowIndex: number) => <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.username ? user.username : 'N/A'}</td>
+                            <td>{user.address ? user.address.city : 'N/A'}</td>
+                            <td>{user.email}</td>
+                            <td>
+                                <CustomButton variant='warning' onClick={() => handleEdit(user.id)}>Edit</CustomButton>
+                            </td>
+                            <td>
+                                <CustomButton variant='danger' onClick={() => handleDelete(user.id)}>Delete</CustomButton>
+                            </td>
+                        </tr>)
+                    }
+                </tbody>
+            </Table>
+            </CardBody>
+        </Card>
     );
 };
 
